3db07d85feae813babfbb3d543d1d28c28e80c3b
sliang <liang20000409@163.com>
2022-08-17 14:30:33 +0800

    Site updated: 2022-08-17 14:30:33

--- js/script.js
+++ js/script.js
@@ -39,32 +39,38 @@ setInterval(() => {
   document.getElementById("runtime").innerHTML = currentTimeHtml;
 }, 1000);
 
-// 新代码
+// 黑夜切换
+document.addEventListener("DOMContentLoaded", function () {
+  const navFn = {
+    switchDarkMode: () => {
+      // Switch Between Light And Dark Mode
+      const nowMode =
+        document.documentElement.getAttribute("data-theme") === "dark"
+          ? "dark"
+          : "light";
+      if (nowMode === "light") {
+        activateDarkMode();
+        saveToLocal.set("theme", "dark", 2);
+        GLOBAL_CONFIG.Snackbar !== undefined &&
+          btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night);
+        document.getElementById("darkmode-switch").innerHTML = " 白天";
+      } else {
+        activateLightMode();
+        saveToLocal.set("theme", "light", 2);
+        GLOBAL_CONFIG.Snackbar !== undefined &&
+          btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day);
+        document.getElementById("darkmode-switch").innerHTML = " 黑夜";
+      }
+      // handle some cases
+      typeof utterancesTheme === "function" && utterancesTheme();
+      typeof FB === "object" && window.loadFBComment();
+      window.DISQUS &&
+        document.getElementById("disqus_thread").children.length &&
+        setTimeout(() => window.disqusReset(), 200);
+    },
+  };
 
-// // 确保其他页面第一个不添加
-if (location.pathname == "/") newPost();
-
-// // 最新文章
-function newPost() {
-  let ls = document.querySelectorAll(".recent-post-info");
-  // 先让时间和索引值都等于第一个的
-  let time = new Date(
-    ls[0].querySelector(".post-meta-date-created").getAttribute("datetime")
-  ).getTime();
-  let index = 0;
-  // 遍历数组，如果有时间比time大的则替换
-  ls.forEach((i, num) => {
-    let t = new Date(
-      i.querySelector(".post-meta-date-created").getAttribute("datetime")
-    ).getTime();
-    if (t > time) {
-      time = t;
-      index = num;
-    }
+  document.getElementById("darkmodeBt").addEventListener("click", function () {
+    navFn.switchDarkMode();
   });
-  // 单数在右，双数在左
-  let className = index % 2 == 0 ? "newPost-right" : "newPost-left";
-  ls[index].innerHTML += '<span class="' + className + '">最 新</span>';
-  // 如果你不想让其一左一右，可以注释上面的启用下面的
-  // ls[index].innerHTML += '<span class="newPost-left">最 新</span>';
-}
+});
