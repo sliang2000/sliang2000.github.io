0d23388a54675643f6e6b25afe517fcb50e8f6d1
Ethan <liang20000409@163.com>
2022-09-21 11:37:29 +0800

    Site updated: 2022-09-21 11:37:29

--- js/universe.js
+++ js/universe.js
@@ -3,36 +3,36 @@ function dark() {
     window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
-    window.msRequestAnimationFrame;
+    window.msRequestAnimationFrame
   var n,
     e,
     i,
     h,
     t = 0.05,
-    s = document.getElementById("universe"),
+    s = document.getElementById('universe'),
     o = !0,
-    a = "180,184,240",
-    r = "226,225,142",
-    d = "226,225,224",
-    c = [];
+    a = '180,184,240',
+    r = '226,225,142',
+    d = '226,225,224',
+    c = []
 
   function f() {
-    (n = window.innerWidth),
+    ;(n = window.innerWidth),
       (e = window.innerHeight),
       (i = 0.216 * n),
-      s.setAttribute("width", n),
-      s.setAttribute("height", e);
+      s.setAttribute('width', n),
+      s.setAttribute('height', e)
   }
   function u() {
-    h.clearRect(0, 0, n, e);
+    h.clearRect(0, 0, n, e)
     for (var t = c.length, i = 0; i < t; i++) {
-      var s = c[i];
-      s.move(), s.fadeIn(), s.fadeOut(), s.draw();
+      var s = c[i]
+      s.move(), s.fadeIn(), s.fadeOut(), s.draw()
     }
   }
   function y() {
-    (this.reset = function () {
-      (this.giant = m(3)),
+    ;(this.reset = function () {
+      ;(this.giant = m(3)),
         (this.comet = !this.giant && !o && m(10)),
         (this.x = l(0, n - 10)),
         (this.y = l(0, e)),
@@ -43,157 +43,155 @@ function dark() {
         (this.fadingIn = !0),
         (this.opacity = 0),
         (this.opacityTresh = l(0.2, 1 - 0.4 * (this.comet + 1 - 1))),
-        (this.do = l(5e-4, 0.002) + 0.001 * (this.comet + 1 - 1));
+        (this.do = l(5e-4, 0.002) + 0.001 * (this.comet + 1 - 1))
     }),
       (this.fadeIn = function () {
         this.fadingIn &&
           ((this.fadingIn = !(this.opacity > this.opacityTresh)),
-          (this.opacity += this.do));
+          (this.opacity += this.do))
       }),
       (this.fadeOut = function () {
         this.fadingOut &&
           ((this.fadingOut = !(this.opacity < 0)),
           (this.opacity -= this.do / 2),
-          (this.x > n || this.y < 0) && ((this.fadingOut = !1), this.reset()));
+          (this.x > n || this.y < 0) && ((this.fadingOut = !1), this.reset()))
       }),
       (this.draw = function () {
         if ((h.beginPath(), this.giant))
-          (h.fillStyle = "rgba(" + a + "," + this.opacity + ")"),
-            h.arc(this.x, this.y, 2, 0, 2 * Math.PI, !1);
+          (h.fillStyle = 'rgba(' + a + ',' + this.opacity + ')'),
+            h.arc(this.x, this.y, 2, 0, 2 * Math.PI, !1)
         else if (this.comet) {
-          (h.fillStyle = "rgba(" + d + "," + this.opacity + ")"),
-            h.arc(this.x, this.y, 1.5, 0, 2 * Math.PI, !1);
+          ;(h.fillStyle = 'rgba(' + d + ',' + this.opacity + ')'),
+            h.arc(this.x, this.y, 1.5, 0, 2 * Math.PI, !1)
           for (var t = 0; t < 30; t++)
             (h.fillStyle =
-              "rgba(" +
+              'rgba(' +
               d +
-              "," +
+              ',' +
               (this.opacity - (this.opacity / 20) * t) +
-              ")"),
+              ')'),
               h.rect(
                 this.x - (this.dx / 4) * t,
                 this.y - (this.dy / 4) * t - 2,
                 2,
                 2
               ),
-              h.fill();
+              h.fill()
         } else
-          (h.fillStyle = "rgba(" + r + "," + this.opacity + ")"),
-            h.rect(this.x, this.y, this.r, this.r);
-        h.closePath(), h.fill();
+          (h.fillStyle = 'rgba(' + r + ',' + this.opacity + ')'),
+            h.rect(this.x, this.y, this.r, this.r)
+        h.closePath(), h.fill()
       }),
       (this.move = function () {
-        (this.x += this.dx),
+        ;(this.x += this.dx),
           (this.y += this.dy),
           !1 === this.fadingOut && this.reset(),
-          (this.x > n - n / 4 || this.y < 0) && (this.fadingOut = !0);
+          (this.x > n - n / 4 || this.y < 0) && (this.fadingOut = !0)
       }),
       setTimeout(function () {
-        o = !1;
-      }, 50);
+        o = !1
+      }, 50)
   }
   function m(t) {
-    return Math.floor(1e3 * Math.random()) + 1 < 10 * t;
+    return Math.floor(1e3 * Math.random()) + 1 < 10 * t
   }
   function l(t, i) {
-    return Math.random() * (i - t) + t;
+    return Math.random() * (i - t) + t
   }
   f(),
-    window.addEventListener("resize", f, !1),
+    window.addEventListener('resize', f, !1),
     (function () {
-      h = s.getContext("2d");
-      for (var t = 0; t < i; t++) (c[t] = new y()), c[t].reset();
-      u();
+      h = s.getContext('2d')
+      for (var t = 0; t < i; t++) (c[t] = new y()), c[t].reset()
+      u()
     })(),
     (function t() {
-      document.getElementsByTagName("html")[0].getAttribute("data-theme") ==
-        "dark" && u(),
-        window.requestAnimationFrame(t);
-    })();
+      document.getElementsByTagName('html')[0].getAttribute('data-theme') ==
+        'dark' && u(),
+        window.requestAnimationFrame(t)
+    })()
 }
-dark();
+dark()
 
 setInterval(() => {
   // let create_time = Math.round(new Date('2021-10-15 00:00:00').getTime() / 1000); //在此行修改建站时间
   // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
-  let create_time = Math.round(
-    new Date("2022/06/20 00:00:00").getTime() / 1000
-  ); //在此行修改建站时间
-  let timestamp = Math.round(new Date().getTime() / 1000);
-  let second = timestamp - create_time;
-  let time = new Array(0, 0, 0, 0, 0);
+  let create_time = Math.round(new Date('2022/06/20 00:00:00').getTime() / 1000) //在此行修改建站时间
+  let timestamp = Math.round(new Date().getTime() / 1000)
+  let second = timestamp - create_time
+  let time = new Array(0, 0, 0, 0, 0)
 
   var nol = function (h) {
-    return h > 9 ? h : "0" + h;
-  };
+    return h > 9 ? h : '0' + h
+  }
   if (second >= 365 * 24 * 3600) {
-    time[0] = parseInt(second / (365 * 24 * 3600));
-    second %= 365 * 24 * 3600;
+    time[0] = parseInt(second / (365 * 24 * 3600))
+    second %= 365 * 24 * 3600
   }
   if (second >= 24 * 3600) {
-    time[1] = parseInt(second / (24 * 3600));
-    second %= 24 * 3600;
+    time[1] = parseInt(second / (24 * 3600))
+    second %= 24 * 3600
   }
   if (second >= 3600) {
-    time[2] = nol(parseInt(second / 3600));
-    second %= 3600;
+    time[2] = nol(parseInt(second / 3600))
+    second %= 3600
   }
   if (second >= 60) {
-    time[3] = nol(parseInt(second / 60));
-    second %= 60;
+    time[3] = nol(parseInt(second / 60))
+    second %= 60
   }
   if (second >= 0) {
-    time[4] = nol(second);
+    time[4] = nol(second)
   }
-  let currentTimeHtml = "";
+  let currentTimeHtml = ''
   if (time[0] != 0) {
-    currentTimeHtml += time[0] + " 年 ";
+    currentTimeHtml += time[0] + ' 年 '
   }
   currentTimeHtml +=
-    "本站已运行:  " +
+    '本站已运行:  ' +
     time[1] +
-    " 天 " +
+    ' 天 ' +
     time[2] +
-    " 时 " +
+    ' 时 ' +
     time[3] +
-    " 分 " +
+    ' 分 ' +
     time[4] +
-    " 秒 ";
-  document.getElementById("runtime").innerHTML = currentTimeHtml;
-}, 1000);
+    ' 秒 '
+  document.getElementById('runtime').innerHTML = currentTimeHtml
+}, 1000)
 
 // 黑夜切换
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   const navFn = {
     switchDarkMode: () => {
       // Switch Between Light And Dark Mode
       const nowMode =
-        document.documentElement.getAttribute("data-theme") === "dark"
-          ? "dark"
-          : "light";
-      if (nowMode === "light") {
-        activateDarkMode();
-        saveToLocal.set("theme", "dark", 2);
+        document.documentElement.getAttribute('data-theme') === 'dark'
+          ? 'dark'
+          : 'light'
+      if (nowMode === 'light') {
+        activateDarkMode()
+        saveToLocal.set('theme', 'dark', 2)
         GLOBAL_CONFIG.Snackbar !== undefined &&
-          btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night);
-        document.getElementById("darkmode-switch").innerHTML = " 白天";
+          btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+        document.getElementById('darkmode-switch').innerHTML = ' 白天'
       } else {
-        activateLightMode();
-        saveToLocal.set("theme", "light", 2);
+        activateLightMode()
+        saveToLocal.set('theme', 'light', 2)
         GLOBAL_CONFIG.Snackbar !== undefined &&
-          btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day);
-        document.getElementById("darkmode-switch").innerHTML = " 黑夜";
+          btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
+        document.getElementById('darkmode-switch').innerHTML = ' 黑夜'
       }
       // handle some cases
-      typeof utterancesTheme === "function" && utterancesTheme();
-      typeof FB === "object" && window.loadFBComment();
+      typeof utterancesTheme === 'function' && utterancesTheme()
+      typeof FB === 'object' && window.loadFBComment()
       window.DISQUS &&
-        document.getElementById("disqus_thread").children.length &&
-        setTimeout(() => window.disqusReset(), 200);
+        document.getElementById('disqus_thread').children.length &&
+        setTimeout(() => window.disqusReset(), 200)
     },
-  };
+  }
 
-  document.getElementById("darkmodeBt").addEventListener("click", function () {
-    navFn.switchDarkMode();
-  });
-});
+  document.getElementById('darkmodeBt').addEventListener('click', function () {
+    navFn.switchDarkMode()
+  })
+})
